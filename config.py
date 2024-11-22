c8c9fe924459fcb9768e3c621a849e083a140c28
arshmusic <bchat837@gmail.com>
2024-11-22 14:16:25 +0530

    Update config.py

--- config.py
+++ config.py
@@ -14,7 +14,7 @@ API_HASH = getenv("API_HASH")
 BOT_TOKEN = getenv("BOT_TOKEN")
 
 # Get your mongo url from cloud.mongodb.com
-MONGO_DB_URI = getenv("MONGO_DB_URI", None)
+MONGO_DB_URI = getenv("MONGO_DB_URI", -1002383330019)
 
 DURATION_LIMIT_MIN = int(getenv("DURATION_LIMIT", 10000))
 
